cee41c8bb021b28f3e0410751b54a70b233a2289
tomwebb2022 <tommyttom10@gmail.com>
2023-03-26 15:15:58 +0100

    task 1.1

--- task-1/main.js
+++ task-1/main.js
@@ -10,3 +10,19 @@ const celebs = [
   "Richard Osman",
   "Emma Thompson"
 ];
+
+//Write a function called `makeLegend`, which takes in a `name`(string) as its only argument
+function makeLegend(name) {
+  return `${name} is a legend`;
+}
+/* The function should return a new string which includes the name plus the 
+words `' is now a legend.'`. For example, if the function was called with the
+ name `'Chris'`, it should return the string `'Chris is now a legend.'`*/
+console.log(makeLegend("Chris"));
+
+/* Using the `makeLegend` function, the `celebs` array 
+and a `for` loop, create a new array which includes `
+' is now a legend.'` for each celebrity. Save this new 
+array to the variable `legendaryCelebs`.*/
+
+
